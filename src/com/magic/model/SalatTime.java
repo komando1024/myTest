abab37820cd040c3a0b7b59d6713c80678beccd2
komando1024 <java.user@laposte.net>
2011-10-08 20:32:25 +0200

    removing a file

--- src/com/magic/model/SalatTime.java
+++ src/com/magic/model/SalatTime.java
@@ -5,50 +5,61 @@ package com.magic.model;
 
 /**
  * @author mejdi
- *
+ * 
  */
 public class SalatTime {
-	
+
 	private String fajr;
 	private String shourouk;
 	private String dhor;
 	private String asr;
 	private String maghrib;
 	private String isha;
-	
+
 	public String getFajr() {
 		return fajr;
 	}
+
 	public void setFajr(String fajr) {
 		this.fajr = fajr;
 	}
+
 	public String getShourouk() {
 		return shourouk;
 	}
+
 	public void setShourouk(String shourouk) {
 		this.shourouk = shourouk;
 	}
+
 	public String getDhor() {
 		return dhor;
 	}
+
 	public void setDhor(String dhor) {
 		this.dhor = dhor;
 	}
+
 	public String getAsr() {
 		return asr;
 	}
+
 	public void setAsr(String asr) {
 		this.asr = asr;
 	}
+
 	public String getMaghrib() {
 		return maghrib;
 	}
+
 	public void setMaghrib(String maghrib) {
 		this.maghrib = maghrib;
 	}
+
 	public String getIsha() {
 		return isha;
 	}
+
 	public void setIsha(String isha) {
 		this.isha = isha;
 	}
